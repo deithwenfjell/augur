f15fa15d3c55b76939d400d334f3a6e9673934df
Dávid Klinovský <david.klinovsky@unitedclassifieds.sk>
2022-12-30 07:00:55 +0100

    ~ predictor

--- src/predictor/predictor.controller.ts
+++ src/predictor/predictor.controller.ts
@@ -1,14 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { PredictorService } from './predictor.service';
+import { IndicatorsDto } from './dto/indicators.dto';
+import { ResultDto } from './dto/result.dto';
 
 @ApiTags('Predictor')
 @Controller('predictor')
+@ApiOkResponse({type: ResultDto })
 export class PredictorController {
     constructor(public readonly predictorService: PredictorService) {}
 
-    @Get()
-    public predict() {
-        return 12345;
+    @Post()
+    public predict(@Body() indicators: IndicatorsDto): ResultDto {
+        return this.predictorService.predict(indicators);
     }
 }
--- src/predictor/predictor.service.ts
+++ src/predictor/predictor.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { IndicatorsDto } from './dto/indicators.dto';
+import { ResultDto } from './dto/result.dto';
 
 @Injectable()
-export class PredictorService {}
+export class PredictorService {
+  public predict(indicators: IndicatorsDto): ResultDto {
+    const res =  new ResultDto();
+    res.result = 12345 + indicators.Age;
+    return res;
+  }
+}
